d52b288f8f78e41dede8f085ab5e96df5e7ebef2
Inhyuk Andy Cho <cih9088@gmail.com>
2025-02-02 15:19:17 +0900

    feat: add flexibility

--- connector/google/google.go
+++ connector/google/google.go
@@ -169,9 +169,10 @@ func (c *googleConnector) Close() error {
 }
 
 func (c *googleConnector) LoginURL(s connector.Scopes, callbackURL, state string) (string, error) {
-	if c.redirectURI != callbackURL {
-		return "", fmt.Errorf("expected callback URL %q did not match the URL in the config %q", callbackURL, c.redirectURI)
-	}
+	// NOTE: I know what I am doing. Commented out for flexibility.
+	// if c.redirectURI != callbackURL {
+	//     return "", fmt.Errorf("expected callback URL %q did not match the URL in the config %q", callbackURL, c.redirectURI)
+	// }
 
 	var opts []oauth2.AuthCodeOption
 	if len(c.hostedDomains) > 0 {
